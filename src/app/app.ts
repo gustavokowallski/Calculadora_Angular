110695a54dbebf3a510925245a04c6c854ad9ab6
Gustavo <kkowakskigustavo@gmail.com>
2025-08-14 14:56:23 -0300

    feat: Criando componente calculadora e adicionando ao app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Calculadora } from './calculadora/calculadora';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
