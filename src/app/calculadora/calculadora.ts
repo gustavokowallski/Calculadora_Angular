46bfc2988e7d41d41fd565093828218bc58b105e
Gustavo <kkowakskigustavo@gmail.com>
2025-08-14 15:09:12 -0300

    feat(calculadora.ts): Criando método e variaveis para calcular resultado

--- src/app/calculadora/calculadora.ts
+++ src/app/calculadora/calculadora.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.scss'
 })
 export class Calculadora {
+    numero1: number =0;
+    numero2: number =0;
+    resultado: number = 0;
+    
 
+    calcularResultado(){
+      console.log("Chamando método para calcular o resultado");
+      console.log("Numero 1" + this.numero1)
+      console.log("Numero 2" + this.numero2)
+      this.resultado = this.numero1+ this.numero2;
+    }
 }
